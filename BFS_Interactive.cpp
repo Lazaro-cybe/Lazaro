b8223a1f30e842f1c1cf5dd508c26356e9406c07
Lazaro-cybe <lazarobaltazary2@gmail.com>
2024-12-24 07:39:22 +0300

    Add files via upload
    Interactive breadth first search (BFS): Allows the user to input the number of nodes, edges, and the graph structure at runtime.

---
+++ BFS_Interactive.cpp
@@ -0,0 +1,52 @@
+// Task 2: Breadth-First Search (BFS) - Interactive
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(int startNode, const vector<vector<int>>& graph) {
+    vector<bool> visited(graph.size(), false);
+    queue<int> q;
+
+    q.push(startNode);
+    visited[startNode] = true;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int nodes, edges;
+    cout << "Enter the number of nodes: ";
+    cin >> nodes;
+
+    vector<vector<int>> graph(nodes + 1);
+
+    cout << "Enter the number of edges: ";
+    cin >> edges;
+    cout << "Enter the edges (node1 node2):" << endl;
+    for (int i = 0; i < edges; ++i) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u); // Remove this line if the graph is directed
+    }
+
+    int startNode;
+    cout << "Enter the starting node for BFS: ";
+    cin >> startNode;
+
+    cout << "BFS starting from node " << startNode << ": ";
+    bfs(startNode, graph);
+    return 0;
+}
